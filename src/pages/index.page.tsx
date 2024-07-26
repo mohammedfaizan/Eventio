20f43231401d3395d91f31d634e8989ac2b5be36
reh21 <rehanmohamed776@gmail.com>
2024-07-26 12:19:47 +0530

    short term fix for dynamic server usage#2

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -5,7 +5,7 @@ import { useCurrentUser } from "src/users/hooks/useCurrentUser"
 import getCurrentUser from "src/users/queries/getCurrentUser"
 
 const Home: BlitzPage = () => {
-  const currentUser = getCurrentUser()
+  const currentUser = useCurrentUser()
 
   return <Layout title="Home">{currentUser && <AuthenticationForm />}</Layout>
 }
